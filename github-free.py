744af16ea1d92b866362cffb176744feb99c7722
qs <mosdit@gmail.com>
2015-02-18 00:43:24 +0300

    refactoring login checker
    Github free login checker refactoring.

--- github-free.py
+++ github-free.py
@@ -4,9 +4,10 @@ from itertools import product
 lst = []
 f = open("valid-github.txt", 'w')
 for i in product('aeiobcdfghmnprst1234567890', repeat=3):
+    name = ''.join(i)
     try:
-        urllib2.urlopen('https://github.com/%s' % ''.join(i))
+        urllib2.urlopen('https://github.com/{0}'.format(name))
     except:
-        f.write('%s\n' % ''.join(i))
-        lst.append(''.join(i))
-        print ''.join(i)
+        f.write('{0}\n'.format(name))
+        lst.append(name)
+        print name
